f0e58e32cf04c6e5f962004c39431c6c63c89738
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-18 16:44:05 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden flex flex-col">
-    <Header />
+    <HeaderSection />
     <main>
       <slot />
     </main>
-    <Footer />
+    <FooterSection />
   </div>
 </template>
