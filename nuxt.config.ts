d851309699c955e48aeaa96f00ed2aca5ac87c53
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-22 13:18:16 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,5 +15,37 @@ export default defineNuxtConfig({
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/icon", "@nuxt/image", "@nuxt/ui-pro", "@nuxt/test-utils", "motion-v/nuxt"],
   uiPro: {
     license: process.env.MY_ENVIRONMENT_VARIABLE
-  }
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+  // To re-enable _all_ Nuxt v3 behavior, set the following options:
+  // srcDir: '.',
+  // dir: {
+  //   app: 'app'
+  // },
+  // experimental: {
+  //   scanPageMeta: 'after-resolve',
+  //   sharedPrerenderData: false,
+  //   compileTemplate: true,
+  //   resetAsyncDataToUndefined: true,
+  //   templateUtils: true,
+  //   relativeWatchPaths: true,
+  //   normalizeComponentNames: false,
+  //   spaLoadingTemplateLocation: 'within',
+  //   parseErrorData: false,
+  //   defaults: {
+  //     useAsyncData: {
+  //       deep: true
+  //     }
+  //   }
+  // },
+  // features: {
+  //   inlineStyles: true
+  // },
+  // unhead: {
+  //   renderSSRHeadOptions: {
+  //     omitLineBreaks: false
+  //   }
+  // }
 });
