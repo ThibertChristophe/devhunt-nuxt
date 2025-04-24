35c7f3eac6e64d895cced5fdab394694d81c0380
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-24 11:00:20 +0200

    Update schema.ts

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,12 +1,12 @@
 import { pgTable, text, integer } from "drizzle-orm/pg-core"
 
 export const candidates = pgTable('candidates', {
-  id: integer('id').primaryKey(),
-  name: text('name').notNull(),
-  email: text('email').notNull().unique(),
-  password: text('password').notNull(),
-  avatar: text('avatar'),
-  createdAt: integer('created_at').notNull(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  email: text().notNull().unique(),
+  password: text().notNull(),
+  avatar: text(),
+  createdAt: timestamp().defaultNow(),
 })
 
 
