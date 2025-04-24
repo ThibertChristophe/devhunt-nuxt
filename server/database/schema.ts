fec08d1ee0494a88a7ccbe7e9ff94f51c01c1104
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-24 15:48:23 +0200

    Update schema.ts

--- server/database/schema.ts
+++ server/database/schema.ts
@@ -1,12 +1,23 @@
-import { pgTable, text, integer } from "drizzle-orm/pg-core"
+import { pgTable, text, integer, varchar, timestamp, boolean } from "drizzle-orm/pg-core"
 
 export const candidates = pgTable('candidates', {
-  id: serial().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 256 }).notNull(),
   email: varchar({ length: 256 }).notNull().unique(),
   password: varchar({ length: 25 }).notNull(),
   avatar: text(),
-  createdAt: timestamp().defaultNow(),
+  createdAt: timestamp('created_at').defaultNow(),
 })
+export type Candidate = typeof candidates.$inferSelect
 
 
+export const jobs = pgTable('jobs', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 256 }).notNull(),
+  description: text(),
+  active: boolean().default(false).notNull(),
+  minSalary: integer('min_salary'),
+  maxSalary: integer('max_salary'),
+  createdAt: timestamp('created_at').defaultNow(),
+})
+export type Job = typeof jobs.$inferSelect
