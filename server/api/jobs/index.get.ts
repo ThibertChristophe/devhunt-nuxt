67ee664bccb27a706fd26229210fec095e5a7854
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-24 16:38:54 +0200

    Update index.get.ts

--- server/api/jobs/index.get.ts
+++ server/api/jobs/index.get.ts
@@ -1,20 +1,30 @@
 import { ilike, tables, useDrizzle, asc } from "~/server/utils/drizzle"
+import { z } from 'zod'
 
-export default eventHandler<{ query: { query: string, page: number } }>(async (event) => {
-    const query = getQuery(event)
-    const pageSize = 6
+const jobSchema = z.object({
+    search: z.string().optional(),
+    page: z.coerce.number(),
+})
+const PAGE_SIZE = 6
 
-    //const jobs = await useDrizzle().select().from(tables.jobs).where(ilike(tables.jobs.description, `%${query.query}%`))
+export default defineEventHandler<{ query: { search: string, page: number } }>(async (event) => {
+    const result = await getValidatedQuery(event, jobSchema.safeParse); // result.data
+    //const query = getQuery(event)
+    //const result = await readValidatedBody(event, userSchema.safeParse) // or `.parse` to directly throw an error
+
+    if (!result.success)
+        throw result.error.issues
+
     const jobs = await useDrizzle().select().from(tables.jobs)
-        .where(query.query ? ilike(tables.jobs.description, `%${query.query}%`) : undefined)
+        .where(result.data.search ? ilike(tables.jobs.description, `%${result.data.search}%`) : undefined)
         .orderBy(asc(tables.jobs.createdAt))
-        .limit(pageSize)
-        .offset((query.page - 1) * pageSize);
+        .limit(PAGE_SIZE)
+        .offset((result.data.page - 1) * PAGE_SIZE);
 
     const res = {
         jobs: jobs,
         pagination: {
-            current_page: query.page,
+            current_page: result.data.page,
             total_count: 2
         }
     }
