03a64bd81312047d5e236c456105e4ac8aed3a4b
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-24 17:12:18 +0200

    Create [id].vue

---
+++ pages/jobs/[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        {{ job }}
+    </div>
+
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+const { data: job } = useFetch(`/api/jobs/${route.params.id}`)
+</script>
