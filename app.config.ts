15ac6476d602a40dee45e7fd70ef23f63a441317
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-25 14:51:22 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,8 @@
 export default defineAppConfig({
   ui: {
+     container: {
+      base: 'w-full max-w-(--ui-container) mx-auto sm:p-6 lg:p-8 bg-gray-800/40 backdrop-blur-sm border border-gray-700 rounded-lg mb-4',
+    },
     colors: {
       primary: 'purple',
       neutral: 'neutral'
