f9f56db5c678ee005b9bc43604aa6c851e234490
Christophe Thibert <82867986+ThibertChristophe@users.noreply.github.com>
2025-04-25 14:38:19 +0200

    Update [id].get.ts

--- server/api/jobs/[id].get.ts
+++ server/api/jobs/[id].get.ts
@@ -1,4 +1,4 @@
-import { tables, useDrizzle } from "~/server/utils/drizzle"
+import { useDrizzle } from "~/server/utils/drizzle"
 import { z } from 'zod'
 
 const jobSchema = z.object({
@@ -11,8 +11,8 @@ export default defineEventHandler(async (event) => {
     if (!result.success)
         throw result.error.issues
 
-    const job = await useDrizzle().select().from(tables.jobs)
-        .where(eq(tables.jobs.id, result.data.id))
-
+    /*const job = await useDrizzle().select().from(tables.jobs)
+        .where(eq(tables.jobs.id, result.data.id))*/
+    const job = await useDrizzle().query.jobs.findFirst({ where: (job, { eq }) => (eq(job.id, result.data.id)) })
     return job
 })
